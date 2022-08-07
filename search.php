da5d780929a4523aca1e1909776ef6c22e5987dc
maki-engineer <ryohei-81@outlook.jp>
2022-08-07 19:06:08 +0900

    update: 色々なメソッドを追加

--- search.php
+++ search.php
@@ -1,7 +1,28 @@
 <?php
 
 class Search{
-  //
+  private $array;
+  private $searchNum;
+
+  public function __construct($array, $searchNum){
+    $this->array     = $array;
+    $this->searchNum = $searchNum;
+  }
+
+  // 線形探索法
+  public function linearSearch(){}
+
+  // 2分探索法
+  public function binarySearch(){}
+
+  // ハッシュ表探索
+  public function HashTableSearch(){}
+
+  // オープンアドレス法
+  public function OpenAddress(){}
+
+  // ハッシュチェイン法
+  public function hashChain(){}
 }
 
 ?>
